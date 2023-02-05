28ec3efb9e6d7d48f72d1c4ec25c9610e8d0c96c
Cruz Flores <cruz.tag@gmail.com>
2023-02-05 17:32:19 -0600

    kangaroo solved

---
+++ problem-solving/number-line-of-jumps-kangaroo.ts
@@ -0,0 +1,19 @@
+function kangaroo(x1: number, v1: number, x2: number, v2: number): string {
+  if (v1 < v2) {
+    return 'NO';
+  }
+  const dist = x2 - x1;
+  const jumps = dist / (v1 - v2);
+  if (jumps % 1 !== 0) {
+    return 'NO';
+  }
+  return 'YES';
+}
+
+function kangarooCGPT(x1: number, v1: number, x2: number, v2: number): string {
+  return v1 < v2 || (x2 - x1) % (v1 - v2) !== 0 ? 'NO' : 'YES';
+}
+
+function kangarooCGPT1(x1: number, v1: number, x2: number, v2: number): string {
+  return v1 > v2 && (x2 - x1) % (v1 - v2) === 0 ? 'YES' : 'NO';
+}
